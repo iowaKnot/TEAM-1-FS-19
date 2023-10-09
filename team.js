efd24662f7bca4236bc66dbc5dada752268f964f
Sabrina Moccia <sabrinamoccia93@outlook.it>
2023-10-09 21:08:14 +0200

    Change finished

--- team.js
+++ team.js
@@ -1,1 +1,13 @@
-const array = [];
+const array = [
+    name = "Sara",
+    surname = "Bianchi",
+    age = 25,
+    city = "Rome",
+    hobby = "gardening",
+    favoriteFood = "carbonara",
+    favoriteVideoGame = "Call of Duty",
+    favoriteFilm = "The Godfather",
+    favoriteBook = "Pride and Prejudice",
+    petName = "Kira",
+
+]
