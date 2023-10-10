5e40677431ce933aae68447e9011aec36570dded
Raffaele <ralf9997@gmail.com>
2023-10-10 09:53:08 +0200

    aggiunta array

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const array = [];
+const array = [name, surname, age, city, hobby, favoriteFood,favoriteVideoGame,
+    favoriteFilm, favoriteBook, petNamegit ];
+   name=Raffaele;
+   surname= Filannino;
+   age= 24;
+   city=Barletta;
+   hobby=football;
+   favoriteFood=Ragu;
+   favoriteVideoGame="Black ops 2";
+   favoriteFilm="signore degli anelli";
+   favoriteBook= "intelligenza emotiva";
+   petName= shila;
