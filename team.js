977777efca7b35720f4e9ecc080907d068fae7ec
Tommy <tommaso.merli97@gmail.com>
2023-10-11 12:06:40 +0200

    initial commit

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const array = [];
+const array = [
+    {
+        name: "Caio",
+        surname: "Merli",
+        age: 26,
+        city: "Milan",
+        hobby: "Video Games",
+        favoriteFood: "Carbonara",
+        favoriteVideoGame: "League Of Legends",
+        favoriteFilm: "Star Wars",
+        favoriteBook: "Bible",
+        petName: "Leia",
+    }
+];
