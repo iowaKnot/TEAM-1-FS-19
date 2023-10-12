e47bf31f07be92c6274e414ad41205ba13f678c0
Raffaele <ralf9997@gmail.com>
2023-10-12 10:26:56 +0200

    modifica array

--- team.js
+++ team.js
@@ -1,12 +1,14 @@
-const array = [name, surname, age, city, hobby, favoriteFood,favoriteVideoGame,
-    favoriteFilm, favoriteBook, petNamegit ];
-   name=Raffaele;
-   surname= Filannino;
-   age= 24;
-   city=Barletta;
-   hobby=football;
-   favoriteFood=Ragu;
-   favoriteVideoGame="Black ops 2";
-   favoriteFilm="signore degli anelli";
-   favoriteBook= "intelligenza emotiva";
-   petName= shila;
+const array=[
+    {
+    name:"Raffaele",
+    surname:"Filannino", 
+    age:24, 
+    city:"Barletta", 
+    hobby:"football", 
+    favoriteFood:"Ragu",
+    favoriteVideoGame:"Black ops 2", 
+    favoriteFilm:"signore degli anelli", 
+    favoriteBook:"intelligenza emotiva", 
+    petName:shila,
+    },
+];
